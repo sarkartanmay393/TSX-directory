6217dcb8a82199213f0443b855346797b126001a
Tanmay Sarkar <sarkartanmay393@gmail.com>
2024-04-29 02:02:28 +0530

    feat: https in vc_be

--- tsx-voicechat/frontend/src/utils/environments.js
+++ tsx-voicechat/frontend/src/utils/environments.js
@@ -1,3 +1,3 @@
 export const BACKEND_URL =
   process.env.BACKEND_URL ||
-  "http://ec2-65-2-9-148.ap-south-1.compute.amazonaws.com:8080";
+  "https://ec2-65-2-9-148.ap-south-1.compute.amazonaws.com";
