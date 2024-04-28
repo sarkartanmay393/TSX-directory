4ecbbc079c240ea7586d5343124c7b3a1e3370e0
Tanmay Sarkar <sarkartanmay393@gmail.com>
2024-04-29 00:33:40 +0530

    feat: BE url changed to EC2

--- tsx-voicechat/frontend/src/utils/environments.js
+++ tsx-voicechat/frontend/src/utils/environments.js
@@ -1,2 +1,3 @@
 export const BACKEND_URL =
-  process.env.BACKEND_URL || "https://tsx-vc-backend.onrender.com";
+  process.env.BACKEND_URL ||
+  "http://ec2-65-2-9-148.ap-south-1.compute.amazonaws.com:8080";
